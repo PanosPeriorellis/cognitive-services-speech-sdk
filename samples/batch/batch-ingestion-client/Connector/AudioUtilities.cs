3228b06baff55be0a8163e1e0253d36244d32056
Panos Periorellis <panos.periorellis@microsoft.com>
2021-06-04 14:16:42 +0200

    Create AudioUtilities.cs

---
+++ samples/batch/batch-ingestion-client/Connector/AudioUtilities.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="AudioUtilities.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+namespace Connector
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class AudioUtilities
+    {
+        internal static int CountAudioChannels(byte[] audioBytes)
+        {
+            return default;
+        }
+
+        internal static byte[] GetAudioChannelByNumber(int channel)
+        {
+            return default;
+        }
+    }
+}
